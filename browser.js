d640402661fd7698b015044534235e77df41ef4c
Vidar Eldøy <vidar@eldoy.com>
2020-01-28 13:15:43 +0100

    Update browser version

--- browser.js
+++ browser.js
@@ -1,23 +1,30 @@
 module.exports = function(url, params, options) {
   return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest()
     if (!options) options = {}
     if (!params) params = {}
-    if (!options.headers) options.headers = {}
+    var xhr = new XMLHttpRequest()
+    xhr.addEventListener('load', function(event) {
+      var json = JSON.parse(xhr.responseText)
+      resolve(json)
+    })
+    xhr.addEventListener('error', function(event){
+      reject(xhr)
+    })
+    xhr.open(options.method || 'POST', url + (options.path || '/'))
 
     // Set up upload if we have files
-    var formData
+    var data
     if (options.files) {
-      formData = new FormData()
+      data = new FormData()
 
-      // Add params to form data
+      // Add params to data
       for (var key in params) {
-        formData.append(key, params[key])
+        data.append(key, params[key])
       }
 
       // Loop through each of the selected files
       for (var file of options.files) {
-        formData.append('file', file, file.name)
+        data.append('file', file, file.name)
       }
 
       if (options.progress) {
@@ -26,33 +33,11 @@ module.exports = function(url, params, options) {
           options.progress(event)
         })
       }
-
-      // Add content type
-      options.headers['content-type'] = 'multipart/form-data'
+    } else {
+      xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8')
     }
-
-    xhr.addEventListener('load', function(event) {
-      var json = JSON.parse(xhr.responseText)
-      resolve(json)
-    })
-
-    xhr.addEventListener('error', function(event){
-      reject(xhr)
-    })
-
-    xhr.open(options.method || 'post', url + (options.path || '/'))
-
-    // Set headers
-    if (!options.headers['content-type']) {
-      options.headers['content-type'] = 'application/json; charset=utf-8'
-    }
-    for (var header in options.headers) {
-      xhr.setRequestHeader(header, options.headers[header])
-    }
-
-    var data = formData || JSON.stringify(params)
 
     // Send data to server
-    xhr.send(data)
+    xhr.send(data || JSON.stringify(params))
   })
 }
